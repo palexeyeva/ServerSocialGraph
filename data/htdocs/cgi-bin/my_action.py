c0729af9ee891c9eb49f3d1cc926e173d86d82f3
DlfMinn <dlfminn675@gmail.com>
2021-04-11 17:02:07 +0300

    сохранены необходимые данные

--- data/htdocs/cgi-bin/my_action.py
+++ data/htdocs/cgi-bin/my_action.py
@@ -21,11 +21,19 @@ vk_session = vk_api.VkApi(f.readline(), f.readline())
 vk_session.auth()
 vk = vk_session.get_api()
 
-advancedSearch=vk.users.search(q = form['user_name'] + ' ' + form['user_surname'],
+searchResults=vk.users.search(q = form['user_name'] + ' ' + form['user_surname'],
  birth_day = int(form['user_bd.day']), birth_month = int(form['user_bd.month']), 
  birth_year = int(form['user_bd.year']), sex = form.getvalue('user_sex'), 
  age_from = int(form['user_ageFrom']), age_to = int(form['user_ageTo']), 
  country = form.getvalue('user_country_S'), city = vk.database.getCities(country_id = form.getvalue('user_country_S'), q= form['user_city']), 
  company = form['user_job_AS'], count = 100, fields='bdate, city, photo_50')
 
-print(html%(advancedSearch)) # вывод полученных данных
+ for i in searchResults["items"]:
+    id = searchResults["items"][i]["id"]
+    name = searchResults["items"][i]["first_name"] + " " + searchResults["items"][i]["last_name"]
+    photo = searchResults["items"][i]["photo_50"]
+    city = searchResults["items"][i]["city"]["title"]
+    bday = searchResults["items"][i]["bdate"]
+    print(id + "\n" + photo + "\n" + name + "\n" + city + "\n" + bday + "\n")
+
+#print(html%(searchResults)) # вывод полученных данных
