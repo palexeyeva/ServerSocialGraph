8c4e0f5771b60ad53e9a65b421357a4e07e7a163
DlfMinn <dlfminn675@gmail.com>
2021-05-04 00:55:39 +0300

    Одноклассники

--- data/htdocs/test.py
+++ data/htdocs/test.py
@@ -11,6 +11,7 @@ import cgi
 import json
 import datetime
 import vk_api 
+import requests
 
 print("test2")
 
@@ -19,12 +20,16 @@ def captcha_handler(captcha):
     key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
     return captcha.try_again(key)
 
-form = cgi.FieldStorage() 
-f = open("C:/login.txt", "r")
-vk_session = vk_api.VkApi(f.readline(), f.readline(), captcha_handler=captcha_handler)
-vk_session.auth()
-vk = vk_session.get_api()
+def getCountryOK(vk):
+  if vk == 228: #ямайка
+    return "10415454380"
+  elif vk == 1: #Россия
+    return "10414533690"
+  elif vk == 2: #Украина
+    return "10424076448"
 
+#Загрузка данных из формы и их форматирование
+form = cgi.FieldStorage() 
 
 name = form.getvalue("name")
 surName = form.getvalue("surName")
@@ -53,6 +58,11 @@ if form.getvalue("city") !=None and country != None :
 else:
     city = None
 
+#ВКонтакте
+f = open("C:/login.txt", "r")
+vk_session = vk_api.VkApi(f.readline(), f.readline(), captcha_handler=captcha_handler)
+vk_session.auth()
+vk = vk_session.get_api()
 searchResults=vk.users.search(q = name + ' ' + surName,  birth_day = bday, birth_month = bmonth,  birth_year = byear, sex = sex,  country = country, city = city, age_from = ageFrom, age_To = ageTo, company = job, count = 100, fields='bdate, city, photo_200_orig, screen_name, is_closed')
 
 print("test3")
@@ -93,6 +103,7 @@ for i in range(len(searchResults['items'])):
         returnResults.append(g)
 # print(returnResults[0][0])
 
+
 f = open('res.txt', 'w', encoding = "utf-8")
 for item in returnResults:
     f.write("%s;%s;%s;%s;%s;%s;%s\n" % (item[0], item[1], item[2], item[3], item[4], item[5], item[6]))
@@ -101,6 +112,102 @@ f.close
 print("test4")
 print ("</body></html>")
 
+#Одноклассники 
+headers = {'Content-Type':'application/json','Cookie': 'bci=5950650924462897331; _statid=4d1a418c-731e-4c3e-bcdf-7791d79340a2; viewport=824; _userIds=""; _suserIds=""; TZ=6; _flashVersion=0; nbp=; msg_conf=2468555756792551; cudr=0; klos=0; LASTSRV=ok.ru; CDN=; AUTHCODE=Jay_C25y0dgJpfLEaZ16qPQxwXyrcQxCD0XSqsRewIT75xx-YSebhWuHjwQeSnnl0j3dlrekAhtTZ5ORmnagEQGW5XRO0ewWPV8Cve8Ui4-5hUvT6CDTpt5K_RPo8QFFp1EtmLNSwtk0UGE_3; JSESSIONID=aea0980ba373a5d38160db32767149118a35ce44f2e573f6.100a24b6; TZD=6.7430; TD=7430'}  
+
+json = {
+  "id": 72,
+  "parameters": {
+    "query": "",
+    "filters": {
+      "st.cmd": "searchResult",
+      "st.mode": "Users",
+      "st.query": "",
+      "st.grmode": "Groups"
+    },
+    "chunk": {
+      "firstIndex": 0,
+      "offset": 0
+    },
+    "prefetch": False
+  }
+}
+
+if sex == 1:
+    sex = "f"
+elif sex == 2:
+    sex = "m"
+else sex = None
+
+json["parameters"]["query"] = name + " " + surName
+json["parameters"]["filters"]["st.query"] = name + " " + surName
+if bday!=None or bmonth!=None or bday!=None:
+  if bday!=None:
+    json["parameters"]["filters"]["st.bthDay"] = str(bday)
+  if bmonth!=None:
+    json["parameters"]["filters"]["st.bthMonth"] = str(bmonth)
+  if byear!=None:
+    json["parameters"]["filters"]["st.bthYear"] = str(byear)
+elif ageTo!=None or ageFrom!=None:
+  if ageTo!=None:
+    json["parameters"]["filters"]["st.tillAge"] = str(ageTo)
+  if ageFrom!=None:
+    json["parameters"]["filters"]["st.fromAge"] = str(ageFrom)
+if country!=None:
+  json["parameters"]["filters"]["st.country"] = str(getCountryOK(country))
+if city!=None:
+  json["parameters"]["filters"]["st.location"] = city
+#print(json)
+
+r = requests.post('https://ok.ru/web-api/v2/search/portal', json=json, headers=headers)
+r=r.json()
+#print(r)
+
+ok = OkApi(access_token='tkn18FhnkdXDJDfPIdcD11DKoY7ydjm3Xab3e3rCll8j43ECQ1PKNvvaLX9En73m7LL6d',
+application_key='CBLFAQJGDIHBABABA',
+application_secret_key='109E7DC8DE5094FD06621330')
+
+returnResults = []
+for i in range(len(r['result']['users']['values']['results'])):
+  g = []
+  id = ''
+  name = ''
+  surname = ''
+  age = ''
+  href = ''
+  photo = ''
+  location = ''
+  href = 'https://ok.ru' + r['result']['users']['values']['results'][i]['user']['info']['shortLink']
+  name = r['result']['users']['values']['results'][i]['user']['info']['firstName']
+  surname = r['result']['users']['values']['results'][i]['user']['info']['lastName']
+  id = r['result']['users']['values']['results'][i]['user']['info']['uid']
+  #bd = ok.users.getInfo(id)
+  #print(bd['birthday'])
+  try:
+    age = r['result']['users']['values']['results'][i]['user']['info']['age'] 
+  except:
+    age=''
+  try:
+    city = r['result']['users']['values']['results'][i]['user']['location']  
+  except:
+    city = ''
+  try:
+    photo = 'https:' + r['result']['users']['values']['results'][i]['user']['imageUrl'] 
+  except:
+    photo = ''
+  
+  g = [name, surname, age, href, photo, city, id]
+
+  #print(g)
+  returnResults.append(g)
+print(returnResults)
+
+
+f = open('res.txt', 'w', encoding = "utf-8")
+f.write("Одноклассники")
+for item in returnResults:
+    f.write("%s;%s;%s;%s;%s;%s;%s\n" % (item[0], item[1], item[2], item[3], item[4], item[5], item[6]))
+f.close
 
 
 
